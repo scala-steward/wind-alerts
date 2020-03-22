ed1992eeb0ed6cf6fbc3fef81d8bfc2ee1184916
Aman Manocha <amanmanocha@yahoo.com>
2020-03-22 17:40:42 +1100

    Return GMT Time

--- src/main/scala/com/uptech/windalerts/status/Main.scala
+++ src/main/scala/com/uptech/windalerts/status/Main.scala
@@ -3,6 +3,7 @@ package com.uptech.windalerts.status
 import cats.effect._
 
 object Main extends IOApp {
+  System.setProperty("user.timezone", "Australia/Sydney")
   override def run(args: List[String]): IO[ExitCode] =
     Backend.start[IO].use(_ => IO.never)
 }
