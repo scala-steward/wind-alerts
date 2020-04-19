162ed9de8d9f89414c8ba37e11770f237c186369
Aman Manocha <amanmanocha@yahoo.com>
2020-04-19 22:49:54 +1000

    Make user premium expired

--- src/main/scala/com/uptech/windalerts/domain/domain.scala
+++ src/main/scala/com/uptech/windalerts/domain/domain.scala
@@ -56,7 +56,7 @@ object domain {
 
     object Registered extends UserType("Registered")
 
-    object RegisteredExpired extends UserType("RegisteredExpired")
+    object PremiumExpired extends UserType("PremiumExpired")
 
     object Trial extends UserType("Trial")
 
@@ -64,11 +64,11 @@ object domain {
 
     object Premium extends UserType("Premium")
 
-    val values = Seq(Registered, RegisteredExpired, Trial, TrialExpired, Premium)
+    val values = Seq(Registered, PremiumExpired, Trial, TrialExpired, Premium)
 
     def apply(value: String): UserType = value match {
       case Registered.value => Registered
-      case RegisteredExpired.value => RegisteredExpired
+      case PremiumExpired.value => PremiumExpired
       case Trial.value => Trial
       case TrialExpired.value => TrialExpired
       case Premium.value => Premium
--- src/main/scala/com/uptech/windalerts/users/UsersService.scala
+++ src/main/scala/com/uptech/windalerts/users/UsersService.scala
@@ -34,15 +34,15 @@ class UserService(userRepo: UserRepositoryAlgebra[IO],
     } yield operationResult
   }
 
-  def makeUserRegisteredExpired(id: String): EitherT[IO, ValidationError, UserT] = {
+  def makeUserPremiumExpired(id: String): EitherT[IO, ValidationError, UserT] = {
     for {
       user <- getUser(id)
-      operationResult <- makeRegisteredExpired(user)
+      operationResult <- makePremiumExpired(user)
     } yield operationResult
   }
 
-  private def makeRegisteredExpired(user: UserT): EitherT[IO, ValidationError, UserT] = {
-    userRepo.update(user.copy(userType = RegisteredExpired.value, nextPaymentAt = -1)).toRight(CouldNotUpdateUserError())
+  private def makePremiumExpired(user: UserT): EitherT[IO, ValidationError, UserT] = {
+    userRepo.update(user.copy(userType = PremiumExpired.value, nextPaymentAt = -1)).toRight(CouldNotUpdateUserError())
   }
 
   private def updateUserType(user: UserT, userType: String, lastPaymentAt: Long = -1, nextPaymentAt: Long = -1): EitherT[IO, ValidationError, UserT] = {
@@ -188,7 +188,7 @@ class UserService(userRepo: UserRepositoryAlgebra[IO],
     if (purchase.expiryTimeMillis > System.currentTimeMillis()) {
       makeUserPremium(user.id, purchase.startTimeMillis, purchase.expiryTimeMillis)
     } else {
-      makeUserRegisteredExpired(user.id)
+      makeUserPremiumExpired(user.id)
     }
   }
 }
