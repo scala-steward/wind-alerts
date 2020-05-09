d06bab1b2ad003d0fa23555a0a0caa068cd15718
Aman Manocha <amanmanocha@yahoo.com>
2020-05-09 10:20:56 +1000

    Log errors

--- src/main/scala/com/uptech/windalerts/domain/HttpErrorHandler.scala
+++ src/main/scala/com/uptech/windalerts/domain/HttpErrorHandler.scala
@@ -5,38 +5,70 @@ import cats.Monad
 import com.uptech.windalerts.users._
 import org.http4s.Response
 import org.http4s.dsl.Http4sDsl
+import org.log4s.getLogger
+
 
 class HttpErrorHandler[F[_] : Monad] extends Http4sDsl[F] {
   val handleThrowable: Throwable => F[Response[F]] = {
-    case e: ValidationError => handleError(e)
-    case e: errors.OperationNotPermitted => Forbidden(e.message)
-    case e: errors.RecordNotFound => NotFound(e.message)
-    case e: errors.HeaderNotPresent => BadRequest(e.message)
-    case e: errors.UserAlreadyRegistered => Conflict(e.message)
-    case everythingElse => InternalServerError(everythingElse.toString)
+    case e: ValidationError => {
+      getLogger.error(e)
+      handleError(e)
+    }
+    case e: errors.OperationNotPermitted => {
+      getLogger.error(e)
+      Forbidden(e.message)
+    }
+    case e: errors.RecordNotFound => {
+      getLogger.error(e)
+      NotFound(e.message)
+    }
+    case e: errors.HeaderNotPresent => {
+      getLogger.error(e)
+      BadRequest(e.message)
+    }
+    case e: errors.UserAlreadyRegistered => {
+      getLogger.error(e)
+      Conflict(e.message)
+    }
+    case everythingElse => {
+      getLogger.error(everythingElse)
+      InternalServerError(everythingElse.toString)
+    }
   }
 
   val handleError: ValidationError => F[Response[F]] = {
-    case UserAlreadyExistsError(email, deviceType) =>
+    case e@UserAlreadyExistsError(email, deviceType) => {
+      getLogger.error(e)
       Conflict(s"The user with email $email for device type $deviceType already exists")
-    case UserAuthenticationFailedError(name) =>
+    }
+    case e@UserAuthenticationFailedError(name) => {
+      getLogger.error(e)
       BadRequest(s"Authentication failed for user $name")
-    case RefreshTokenNotFoundError() =>
+    }
+    case e@RefreshTokenNotFoundError() => {
+      getLogger.error(e)
       BadRequest(s"Refresh token not found")
-    case RefreshTokenExpiredError() =>
+    }
+    case e@RefreshTokenExpiredError() => {
+      getLogger.error(e)
       BadRequest(s"Refresh token expired")
-    case AlertNotFoundError() =>
+    }
+    case e@AlertNotFoundError() => {
+      getLogger.error(e)
       NotFound("Alert not found")
-    case OperationNotAllowed(message) =>
+    }
+    case e@OperationNotAllowed(message) => {
+      getLogger.error(e)
       Forbidden(message)
-    case OtpNotFoundError() =>
+    }
+    case e@OtpNotFoundError() => {
+      getLogger.error(e)
       Forbidden("Invalid or expired OTP")
-    case e: errors.OperationNotPermitted => Forbidden(e.message)
-    case e: errors.RecordNotFound => NotFound(e.message)
-    case e: errors.HeaderNotPresent => BadRequest(e.message)
-    case e: errors.UserAlreadyRegistered => Conflict(e.message)
-    case everythingElse => InternalServerError(everythingElse.toString)
-    case everythingElse => InternalServerError(everythingElse.toString)
+    }
+    case e@everythingElse => {
+      getLogger.error(e)
+      InternalServerError(everythingElse.toString)
+    }
   }
 
 }
