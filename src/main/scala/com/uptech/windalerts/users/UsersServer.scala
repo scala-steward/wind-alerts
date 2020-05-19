958dd262d289f874834ec8d1ca7953d4465d25ee
Aman Manocha <amanmanocha@yahoo.com>
2020-05-19 21:44:10 +1000

    Do not create credential object

--- src/main/scala/com/uptech/windalerts/users/UsersServer.scala
+++ src/main/scala/com/uptech/windalerts/users/UsersServer.scala
@@ -24,21 +24,19 @@ import org.mongodb.scala.{MongoClient, MongoCollection}
 import scala.util.Try
 
 object UsersServer extends IOApp {
-  val started = System.currentTimeMillis()
-  sys.addShutdownHook(getLogger.error(s"Shutting down after ${(System.currentTimeMillis() - started)} ms"))
+
   implicit val backend = HttpURLConnectionBackend()
 
   override def run(args: List[String]): IO[ExitCode] = for {
     _ <- IO(getLogger.error("Starting"))
 
     projectId <- IO(sys.env("projectId"))
-    credentials <- IO(Try(GoogleCredentials.fromStream(new FileInputStream(s"/app/resources/$projectId.json")))
-      .getOrElse(GoogleCredentials.getApplicationDefault))
     applePrivateKey <- IO(Try(AppleLogin.getPrivateKey(s"/app/resources/Apple-$projectId.p8"))
       .getOrElse(AppleLogin.getPrivateKey(s"src/main/resources/Apple.p8")))
 
     client <- IO.pure(MongoClient(com.uptech.windalerts.domain.secrets.read.surfsUp.mongodb.url))
     mongoDb <- IO(client.getDatabase(sys.env("projectId")).withCodecRegistry(com.uptech.windalerts.domain.codecs.codecRegistry))
+
     otpColl  <- IO( mongoDb.getCollection[OTPWithExpiry]("otp"))
     otpRepo <- IO( new MongoOtpRepository(otpColl))
 
@@ -52,11 +50,11 @@ object UsersServer extends IOApp {
     credentialsRepository <- IO( new MongoCredentialsRepository(credentialsCollection))
 
     androidPublisher <- IO(AndroidPublisherHelper.init(ApplicationConfig.APPLICATION_NAME, ApplicationConfig.SERVICE_ACCOUNT_EMAIL))
+
     androidPurchaseRepoColl  <- IO( mongoDb.getCollection[AndroidToken]("androidPurchases"))
+    androidPurchaseRepo <- IO( new MongoAndroidPurchaseRepository(androidPurchaseRepoColl))
 
     applePurchaseRepoColl  <- IO( mongoDb.getCollection[AppleToken]("applePurchases"))
-
-    androidPurchaseRepo <- IO( new MongoAndroidPurchaseRepository(androidPurchaseRepoColl))
     applePurchaseRepo <- IO( new MongoApplePurchaseRepository(applePurchaseRepoColl))
 
     fbcredentialsCollection  <- IO( mongoDb.getCollection[FacebookCredentialsT]("facebookCredentials"))
@@ -65,28 +63,27 @@ object UsersServer extends IOApp {
     appleCredentialsCollection  <- IO( mongoDb.getCollection[AppleCredentials]("appleCredentials"))
     appleCredentialsRepository <- IO( new MongoAppleCredentialsRepositoryAlgebra(appleCredentialsCollection))
 
-    notificationsColl <-  IO(mongoDb.getCollection[Notification]("notifications"))
-    notificationsRepository <-  IO(new MongoNotificationsRepository(notificationsColl))
 
     feedbackColl  <- IO( mongoDb.getCollection[Feedback]("feedbacks"))
     feedbackRepository <- IO( new MongoFeedbackRepository(feedbackColl))
 
     alertsCollection  <- IO( mongoDb.getCollection[AlertT]("alerts"))
     alertsRepository <- IO( new MongoAlertsRepositoryAlgebra(alertsCollection))
+
     emailConf = com.uptech.windalerts.domain.secrets.read.surfsUp.email
     emailSender = new EmailSender(emailConf.userName, emailConf.password)
 
     usersService <- IO(new UserService(userRepository, credentialsRepository, appleCredentialsRepository, fbcredentialsRepository, refreshTokenRepo, alertsRepository, feedbackRepository, secrets.read.surfsUp.facebook.key, androidPublisher, applePrivateKey, emailSender))
     auth <- IO(new Auth(refreshTokenRepo))
     apiKey <- IO(secrets.read.surfsUp.willyWeather.key)
+
     beaches <- IO(new BeachService[IO](new WindsService[IO](apiKey), new TidesService[IO](apiKey), new SwellsService[IO](apiKey, swellAdjustments.read)))
     httpErrorHandler <- IO(new HttpErrorHandler[IO])
 
     endpoints <- IO(new UsersEndpoints(usersService, httpErrorHandler, refreshTokenRepo, otpRepo, androidPurchaseRepo, applePurchaseRepo, auth))
-    alertService <- IO(new AlertsService[IO](alertsRepository))
 
+    alertService <- IO(new AlertsService[IO](alertsRepository))
     alertsEndPoints <- IO(new AlertsEndpoints(alertService, usersService, auth, httpErrorHandler))
-    //notifications <- IO(new Notifications(alerts, beachesService, beachSeq, usersRepo, dbWithAuth._2, httpErrorHandler, notificationsRepository, config = config.read))
 
     httpApp <- IO(errors.errorMapper(Logger.httpApp(false, true, logAction = requestLogger)(
       Router(
@@ -94,11 +91,8 @@ object UsersServer extends IOApp {
         "/v1/users" -> endpoints.openEndpoints(),
         "/v1/users/social/facebook" -> endpoints.facebookEndpoints(),
         "/v1/users/social/apple" -> endpoints.appleEndpoints(),
-
         "/v1/users/alerts" -> auth.middleware(alertsEndPoints.allUsersService()),
-        "/v1/notify" ->
-          SendNotifications.routes(alertService, beachesService, userRepository, dbWithAuth._2, httpErrorHandler),
-        "" -> Routes[IO](beaches, new HttpErrorHandler[IO]).allRoutes(),
+        "" -> Routes[IO](beaches, httpErrorHandler).allRoutes(),
       ).orNotFound)))
     server <- BlazeServerBuilder[IO]
                   .bindHttp(sys.env("PORT").toInt, "0.0.0.0")
