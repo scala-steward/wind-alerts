89265fccfd2cbbc8d31494bec68da7a0f26ab14e
Aman Manocha <amanmanocha@yahoo.com>
2020-12-22 21:35:13 +0530

    Logging requests

--- src/main/scala/com/uptech/windalerts/domain/logger.scala
+++ src/main/scala/com/uptech/windalerts/domain/logger.scala
@@ -5,6 +5,6 @@ import org.log4s.getLogger
 
 object logger {
   def requestLogger:Some[String => IO[Unit]] = {
-    Some(msg => IO(getLogger.error(msg)))
+    Some(msg => IO(getLogger.error("Request : " + msg)))
   }
 }
