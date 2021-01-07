d2de776ea4850a49f337660063b22cea58559cba
Aman Manocha <amanmanocha@yahoo.com>
2021-01-07 14:35:31 +0530

    Some log information

--- src/main/scala/com/uptech/windalerts/notifications/Notifications.scala
+++ src/main/scala/com/uptech/windalerts/notifications/Notifications.scala
@@ -30,15 +30,17 @@ class Notifications(A: AlertsService[IO], B: BeachService[IO], beaches: Map[Long
       beaches                         <- B.getAll(alertsByBeaches.keys.toSeq)
       x                               =  alertsByBeaches.map(kv => (beaches(kv._1), kv._2))
       alertsToBeNotified              =  x.map(kv => (kv._1, kv._2.filter(_.isToBeNotified(kv._1)).map(a => domain.AlertWithBeach(a, kv._1))))
-      _                               <- EitherT.liftF(IO(logger.error(s"alertsToBeNotified ${alertsToBeNotified.mkString}")))
+      _                               <- EitherT.liftF(IO(logger.error(s"alertsToBeNotified ${alertsToBeNotified.map(_._2.map(_.alert._id)).mkString}")))
       usersToBeNotified               <- alertsToBeNotified.values.flatten.map(v => repos.usersRepo().getByUserIdEitherT(v.alert.owner)).toList.sequence
       userIdToUser                    =  usersToBeNotified.map(u => (u._id.toHexString, u)).toMap
       alertWithUserWithBeach          =  alertsToBeNotified.values.flatten.map(v => AlertWithUserWithBeach(v.alert, userIdToUser(v.alert.owner), v.beach))
       _                               <- EitherT.liftF(IO(logger.error(s"alertWithUserWithBeach ${alertWithUserWithBeach.map(_.alert._id).mkString}")))
       usersToBeDisabledAlertsFiltered =  alertWithUserWithBeach.filterNot(f => f.user.disableAllAlerts)
+      _                               <- EitherT.liftF(IO(logger.error(s"usersToBeDisabledAlertsFiltered ${usersToBeDisabledAlertsFiltered.map(_.alert._id).mkString}")))
       usersToBeNotifiedSnoozeFiltered =  usersToBeDisabledAlertsFiltered.filterNot(f => f.user.snoozeTill > System.currentTimeMillis())
+      _                               <- EitherT.liftF(IO(logger.error(s"usersToBeNotifiedSnoozeFiltered ${usersToBeNotifiedSnoozeFiltered.map(_.alert._id).mkString}")))
       loggedOutUserFiltered           =  usersToBeNotifiedSnoozeFiltered.filterNot(f => f.user.deviceToken == null || f.user.deviceToken.isEmpty())
-      a                               = EitherT.liftF(IO(logger.error(s"loggedOutUserFiltered ${loggedOutUserFiltered.map(_.alert._id).mkString}")))
+      _                               <- EitherT.liftF(IO(logger.error(s"loggedOutUserFiltered ${loggedOutUserFiltered.map(_.alert._id).mkString}")))
       usersWithCounts                 <- loggedOutUserFiltered.map(u => repos.notificationsRepo().countNotificationInLastHour(u.user._id.toHexString)).toList.sequence
       usersWithCountsMap              =  usersWithCounts.map(u => (u.userId, u.count)).toMap
       usersToBeFilteredWithCount      =  loggedOutUserFiltered.filter(u => usersWithCountsMap(u.user._id.toHexString) < u.user.notificationsPerHour)
