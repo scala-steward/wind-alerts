b60ab3a3dbc756876144da4080ada4b0d2c9a01b
Aman Manocha <amanmanocha@yahoo.com>
2021-11-23 13:59:30 +1100

    Delete OTP after verification

--- src/main/scala/com/uptech/windalerts/core/otp/OTPService.scala
+++ src/main/scala/com/uptech/windalerts/core/otp/OTPService.scala
@@ -35,7 +35,6 @@ class OTPService[F[_] : Sync](otpRepository: OtpRepository[F], emailSender: Emai
       otp <- M.pure(createOtp(4))
       _ <- otpRepository.updateForUser(userId, OTPWithExpiry(otp, System.currentTimeMillis() + 5 * 60 * 1000, userId))
       result <- emailSender.sendOtp(email, otp)
-      _ <- otpRepository.deleteForUser(userId)
     } yield result
   }
 
